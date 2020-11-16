02d980feeade946f6ae74bac45609fbd94076747
Simon Walker <s.r.walker101@googlemail.com>
2020-11-16 13:44:57 +0000

    Extracts higher level AwsError

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,29 @@
 use rusoto_cloudformation::DescribeStackEventsError;
 use rusoto_core::{request::BufferedHttpResponse, RusotoError};
 use serde::Deserialize;
-use serde_xml_rs::from_str;
 use std::str::FromStr;
+
+#[derive(PartialEq, Eq, Debug)]
+pub(crate) enum AwsError {
+    CredentialExpired,
+    RateLimitExceeded,
+}
+
+impl FromStr for AwsError {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let error_response = ErrorResponse::from_str(s)?;
+        match error_response.error.code.as_str() {
+            "Throttling" => Ok(Self::RateLimitExceeded),
+            "ExpiredToken" => Ok(Self::CredentialExpired),
+            _ => Err(format!(
+                "unknown response error type: {}",
+                error_response.error.code
+            )),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Deserialize)]
 pub(crate) struct ErrorResponse {
@@ -53,26 +74,82 @@ impl std::fmt::Display for Error {
 mod tests {
     use super::*;
 
-    #[test]
-    fn parse_rate_limiting_error() {
-        let message =
-            "<ErrorResponse xmlns=\"http://cloudformation.amazonaws.com/doc/2010-05-15/\">
+    mod aws_error {
+        use super::*;
+
+        #[test]
+        fn parse_rate_limiting_error() {
+            let message = rate_limiting_message();
+
+            let expected = AwsError::RateLimitExceeded;
+
+            assert_eq!(AwsError::from_str(message).unwrap(), expected);
+        }
+
+        #[test]
+        fn parse_expired_token() {
+            let message = expired_token_message();
+
+            let expected = AwsError::CredentialExpired;
+
+            assert_eq!(AwsError::from_str(message).unwrap(), expected);
+        }
+    }
+
+    mod error_response {
+        use super::*;
+
+        #[test]
+        fn parse_rate_limiting_error() {
+            let message = rate_limiting_message();
+            let expected = ErrorResponse {
+                error: ErrorDetail {
+                    message: "Rate exceeded".to_string(),
+                    code: "Throttling".to_string(),
+                    type_: "Sender".to_string(),
+                },
+                request_id: "989a8c1f-735f-443e-8a77-ac87abf2b027".to_string(),
+            };
+            let error = ErrorResponse::from_str(message).unwrap();
+            assert_eq!(error, expected);
+        }
+
+        #[test]
+        fn parse_expired_token() {
+            let message = expired_token_message();
+
+            let expected = ErrorResponse {
+                error: ErrorDetail {
+                    message: "The security token included in the request is expired".to_string(),
+                    code: "ExpiredToken".to_string(),
+                    type_: "Sender".to_string(),
+                },
+                request_id: "4a281eb4-4572-4906-8c3c-e2b1551e2868".to_string(),
+            };
+            let error = ErrorResponse::from_str(message).unwrap();
+            assert_eq!(error, expected);
+        }
+    }
+
+    fn expired_token_message() -> &'static str {
+        "<ErrorResponse xmlns=\"http://cloudformation.amazonaws.com/doc/2010-05-15/\">
+            <Error>
+                <Type>Sender</Type>
+                <Code>ExpiredToken</Code>
+                <Message>The security token included in the request is expired</Message>
+            </Error>
+            <RequestId>4a281eb4-4572-4906-8c3c-e2b1551e2868</RequestId>
+            </ErrorResponse>"
+    }
+
+    fn rate_limiting_message() -> &'static str {
+        "<ErrorResponse xmlns=\"http://cloudformation.amazonaws.com/doc/2010-05-15/\">
             <Error>
                 <Type>Sender</Type>
                 <Code>Throttling</Code>
                 <Message>Rate exceeded</Message>
             </Error>
             <RequestId>989a8c1f-735f-443e-8a77-ac87abf2b027</RequestId>
-        </ErrorResponse>";
-        let expected = ErrorResponse {
-            error: ErrorDetail {
-                message: "Rate exceeded".to_string(),
-                code: "Throttling".to_string(),
-                type_: "Sender".to_string(),
-            },
-            request_id: "989a8c1f-735f-443e-8a77-ac87abf2b027".to_string(),
-        };
-        let error = ErrorResponse::from_str(message).unwrap();
-        assert_eq!(error, expected);
+        </ErrorResponse>"
     }
 }
